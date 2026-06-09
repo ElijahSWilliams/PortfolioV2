646b2f682d48d453bc3c9eb9839775eec988350a
Elijah <xrealntrue16x@gmail.com>
2026-06-09 18:21:19 -0400

    Hamburger Menu added for mobile

--- src/Header/Header.jsx
+++ src/Header/Header.jsx
@@ -1,51 +1,39 @@
+import { useState } from "react";
 import "./Header.css"; 
-
-
-
 
 /* UPDATE PORTFOLIO LINK IN RESUME*/
 /* MAKE RESPONSIVE ON MOBILE DEVICES */
 
 
 function Header() {
-return(
-    <div className="header">
-         <nav className="header__nav">
-     {/*    <a
-          href="#home"
-          className="header__link"
-        >
-          Home
-        </a> */}
-        <a
-          href="#about"
-          className="header__link"
-        >
-          About
-        </a>
-        <a
-          href="#projects"
-          className="header__link"
-        >
-          Projects
-        </a>
-        <a
-          href="#contact"
-          className="header__link"
-        >
-          Contact
-        </a>
-        <a
-          href="/PortfolioV2/FINALIZED_TECH_RESUME(V3F).pdf"
-          download
-          className="header__link"
-        >
-          Download Resume
-        </a>
-      </nav>
-    </div>
-)
-}
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
 
+  return (
+    <header className="header">
+
+      <button
+        className="hamburger"
+        onClick={() => setIsMenuOpen(!isMenuOpen)}
+      >
+        ☰
+      </button>
+
+      <nav className={`header__nav ${isMenuOpen ? "active" : ""}`}>
+            <a href="#about" className="header__link">About</a>
+            <a href="#projects" className="header__link">Projects</a>
+            <a href="#contact" className="header__link">Contact</a>
+           <a
+              href="/PortfolioV2/FINALIZED_TECH_RESUME(V3F).pdf"
+              download
+              className="header__link"
+            >
+              Download Resume
+            </a>
+          
+      </nav>
+
+    </header>
+  );
+}
 
 export default Header;
